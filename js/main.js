5dff9998b8211d70c209d18b7c71a05b92d26d56
Katy DeCorah <kdecorah@gmail.com>
2014-05-07 19:38:09 -0400

    cleaning up code

--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
-var myApp = angular.module('myApp', [], function($interpolateProvider) {
+var myApp = angular.module('staticMapMaker', [], function($interpolateProvider) {
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
 });
 
 var controller = function($scope) {
-  $scope.e = {
-    location:"Albany, NY",
-    locationAbout:"Try an address, a city, a place, or even latitude and longitude.",
-    API:"",
-    APIAbout:"You don't always need an API Key for Static Maps, but it's easy to acquire one. Without a key you might receive an error image instead of a map. Follow the link to the API Console.",
-    zoom:13,
-    minZoom:0,
-    maxZoom:22,
-    scaleAbout:"Scale will double the stated height and width. This is good for when you need a width or height larger than 640px.",
-    width:600,
-    height:300,
-    maxSize:640,
-    sizeAbout:"Max is 640px or 1280px when scale 2x.",
+  $scope.base = {
+    location: "Albany, NY",
+    zoom: 13,
+    minZoom: 0,
+    maxZoom: 22,
+    width: 600,
+    height: 300,
+    maxSize: 640,
     markerColor: "red",
     mapType: "roadmap",
-    format:"png",
-    markerSize:"mid",
-    gimmeAbout:"Treat this like a regular image. Pop it into an img tag or use as a background-image."
+    format: "png",
+    markerSize: "mid",
   };
   $scope.colors =
   [
