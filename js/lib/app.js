27e5de8902382220c042facc9e80ef57bb44e1f4
Katy DeCorah <katydecorah@gmail.com>
2014-12-28 16:04:02 -0500

    updates angular version

--- js/lib/app.js
+++ js/lib/app.js
@@ -3,7 +3,8 @@ var myApp = angular.module('staticMapMaker', [], function ($interpolateProvider)
   $interpolateProvider.endSymbol(']]');
 });
 
-var googleController = function ($scope) {
+myApp.controller('googleController', ['$scope', function($scope) {
+
   $scope.base = {
     location: "Albany, NY",
     zoom: 13,
@@ -20,15 +21,15 @@ var googleController = function ($scope) {
   };
   $scope.colors = [ "black", "brown", "green", "purple", "yellow", "blue", "gray", "orange", "red", "white" ];
   $scope.markerSizes = [
-    { "value": "tiny", "text": "small" },
-    { "value": "small", "text": "medium" },
-    { "value": "mid", "text": "large" }
-  ];
-  $scope.mapTypes = [ "roadmap", "terrain", "satellite", "hybrid" ];
-  $scope.formats = [ "png", "gif", "jpg" ];
-};
+{ "value": "tiny", "text": "small" },
+{ "value": "small", "text": "medium" },
+{ "value": "mid", "text": "large" }
+];
+$scope.mapTypes = [ "roadmap", "terrain", "satellite", "hybrid" ];
+$scope.formats = [ "png", "gif", "jpg" ];
+}]);
 
-var mapboxController = function ($scope) {
+myApp.controller('mapboxController', ['$scope', function($scope) {
   $scope.base = {
     location: "-73.7638,42.6564",
     zoom: 14,
@@ -46,31 +47,34 @@ var mapboxController = function ($scope) {
     markerLabel: "heart"
   };
   $scope.markerSizes = [
-    { "value": "s", "text": "small" },
-    { "value": "m", "text": "medium" },
-    { "value": "l", "text": "large" }
-  ];
-  $scope.labels = [ "airfield", "airport", "alcohol-shop", "america-football", "art-gallery", "bakery", "bank", "bar", "baseball", "basketball", "beer", "bicycle", "building", "bus", "cafe", "camera", "campsite", "car", "cemetery", "chemist", "cinema", "circle-stroked", "circle", "city", "clothing-store", "college", "commercial", "cricket", "cross", "dam", "danger", "disability", "dog-park", "embassy", "emergency-telephone", "entrance", "farm", "fast-food", "ferry", "fire-station", "fuel", "garden", "golf", "grocery", "hairdresser", "harbor", "heart", "heliport", "hospital", "industrial", "land-use", "laundry", "library", "lighthouse", "lodging", "logging", "london-underground", "marker-stroked", "marker", "minefield", "mobilephone", "monument", "museum", "music", "oil-well", "park2", "park", "parking-garage", "parking", "pharmacy", "pitch", "place-of-worship", "playground", "police", "polling-place", "post", "prison", "rail-above", "rail-light", "rail-metro", "rail-underground", "rail", "religious-christian", "religious-jewish", "religious-muslim", "restaurant", "roadblock", "rocket", "school", "scooter", "shop", "skiing", "slaughterhouse", "soccer", "square-stroked", "square", "star-stroked", "star", "suitcase", "swimming", "telephone", "tennis", "theatre", "toilets", "town-hall", "town", "triangle-stroked", "triangle", "village", "warehouse", "waste-basket", "water", "wetland", "zoo", "a", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
-  $scope.formats = [ "png", "png32", "png64", "png128", "png256", "jpg70", "jpg80", "jpg90", "@2x.png" ];
-};
+{ "value": "s", "text": "small" },
+{ "value": "m", "text": "medium" },
+{ "value": "l", "text": "large" }
+];
+$scope.labels = [ "airfield", "airport", "alcohol-shop", "america-football", "art-gallery", "bakery", "bank", "bar", "baseball", "basketball", "beer", "bicycle", "building", "bus", "cafe", "camera", "campsite", "car", "cemetery", "chemist", "cinema", "circle-stroked", "circle", "city", "clothing-store", "college", "commercial", "cricket", "cross", "dam", "danger", "disability", "dog-park", "embassy", "emergency-telephone", "entrance", "farm", "fast-food", "ferry", "fire-station", "fuel", "garden", "golf", "grocery", "hairdresser", "harbor", "heart", "heliport", "hospital", "industrial", "land-use", "laundry", "library", "lighthouse", "lodging", "logging", "london-underground", "marker-stroked", "marker", "minefield", "mobilephone", "monument", "museum", "music", "oil-well", "park2", "park", "parking-garage", "parking", "pharmacy", "pitch", "place-of-worship", "playground", "police", "polling-place", "post", "prison", "rail-above", "rail-light", "rail-metro", "rail-underground", "rail", "religious-christian", "religious-jewish", "religious-muslim", "restaurant", "roadblock", "rocket", "school", "scooter", "shop", "skiing", "slaughterhouse", "soccer", "square-stroked", "square", "star-stroked", "star", "suitcase", "swimming", "telephone", "tennis", "theatre", "toilets", "town-hall", "town", "triangle-stroked", "triangle", "village", "warehouse", "waste-basket", "water", "wetland", "zoo", "a", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
+$scope.formats = [ "png", "png32", "png64", "png128", "png256", "jpg70", "jpg80", "jpg90", "@2x.png" ];
+}]);
 
 //http://stackoverflow.com/a/16388643
 myApp.directive('capitalize', function() {
-   return {
-     require: 'ngModel',
-     link: function(scope, element, attrs, modelCtrl) {
-        var capitalize = function(inputValue) {
-           var capitalized = inputValue.toUpperCase();
-           if(capitalized !== inputValue) {
-              modelCtrl.$setViewValue(capitalized);
-              modelCtrl.$render();
-            }
-            return capitalized;
-         }
-         modelCtrl.$parsers.push(capitalize);
-         capitalize(scope[attrs.ngModel]);  // capitalize initial value
-     }
-   };
+  return {
+    restrict: 'A', // only activate on element attribute
+    require: '?ngModel',
+    link : function(scope, element, attrs, modelCtrl) {
+      var capitalize = function(inputValue) {
+        if(inputValue) {
+          var capitalized = inputValue.toUpperCase();
+          if (capitalized !== inputValue) {
+            modelCtrl.$setViewValue(capitalized);
+            modelCtrl.$render();
+          }
+          return capitalized;
+        }
+      };
+      modelCtrl.$parsers.push(capitalize);
+      capitalize(scope[attrs.ngModel]); // capitalize initial value
+    }
+  };
 });
 
 myApp.filter('nospace', function () {
