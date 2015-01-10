33c21db44e7bc78b516a0be9ac469270233242f3
Katy DeCorah <katydecorah@gmail.com>
2015-01-10 11:58:15 -0500

    adds marker

--- js/lib/app.js
+++ js/lib/app.js
@@ -67,10 +67,10 @@ myApp.controller('mapquestController', ['$scope', function($scope) {
     width: 600,
     height: 300,
     maxSize: 3840,
-    markerColor: "red",
+    markerColor: "blue",
     mapType: "map",
     format: "png",
-    markerSize: "mid",
+    //markerSize: "mid",
     autoCenter: false,
     scaleBar: false,
     showTraffic: false,
@@ -81,12 +81,7 @@ myApp.controller('mapquestController', ['$scope', function($scope) {
     { "value": "con", "text": "construction" },
     { "value": "inc", "text": "incidents" }
   ];
-$scope.colors = [ "blue","green","orange","purple","white","yellow","blue_1","blue_2","blue_3","blue_4","green_1","green_2","green_3","green_4","orange_1","orange_2","orange_3","pink_1","pink_2","pink_3","purple_1","purple_2","purple_3","purple_4","white_1","yellow_1","yellow_2","yellow_3" ];
-$scope.markerSizes = [
-{ "value": "tiny", "text": "small" },
-{ "value": "small", "text": "medium" },
-{ "value": "mid", "text": "large" }
-];
+$scope.colors = ["blue","green","orange","purple","white","yellow","blue_1","blue_2","blue_3","blue_4","bluegreen_1","bluegreen_2","bluegreen_3","green_1","green_2","green_3","green_4","orange_1","orange_2","orange_3","pink_1","pink_2","pink_3","purple_1","purple_2","purple_3","purple_4","red_1","red_2","white_1","yellow_1","yellow_2","yellow_3"];
 $scope.mapTypes = [ "map", "sat", "hyb" ];
 $scope.formats = [ "png", "gif", "jpg", "jpeg" ];
 }]);
