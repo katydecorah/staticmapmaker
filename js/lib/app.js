bd765f5ff4ba144da45bcdd9da440f5d56855dad
Katy DeCorah <katydecorah@gmail.com>
2015-01-10 12:01:08 -0500

    better text on map types

--- js/lib/app.js
+++ js/lib/app.js
@@ -82,7 +82,13 @@ myApp.controller('mapquestController', ['$scope', function($scope) {
     { "value": "inc", "text": "incidents" }
   ];
 $scope.colors = ["blue","green","orange","purple","white","yellow","blue_1","blue_2","blue_3","blue_4","bluegreen_1","bluegreen_2","bluegreen_3","green_1","green_2","green_3","green_4","orange_1","orange_2","orange_3","pink_1","pink_2","pink_3","purple_1","purple_2","purple_3","purple_4","red_1","red_2","white_1","yellow_1","yellow_2","yellow_3"];
-$scope.mapTypes = [ "map", "sat", "hyb" ];
+$scope.mapTypes = [
+
+{ "value": "map", "text": "map" },
+{ "value": "sat", "text": "satellite" },
+{ "value": "hyb", "text": "hybrid" }
+
+ ];
 $scope.formats = [ "png", "gif", "jpg", "jpeg" ];
 }]);
 
