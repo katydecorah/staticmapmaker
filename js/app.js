dfb434362dc9a45b5d3a36dc8960cafb6c5b2e14
Katy DeCorah <katydecorah@gmail.com>
2016-01-01 13:49:09 -0500

    :moneybag:

--- js/app.js
+++ js/app.js
@@ -49,3 +49,13 @@ myApp.directive('capitalize', function() {
     }
   };
 });
+
+myApp.directive('myAdSense', function() {
+  return {
+    restrict: 'A',
+    transclude: true,
+    replace: true,
+    template: '<div ng-transclude></div>',
+    link: function ($scope, element, attrs) {}
+  }
+});
