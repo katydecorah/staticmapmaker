71705e9dea52e9c023ac4068720378be7c9ed2cb
Katy DeCorah <katydecorah@gmail.com>
2022-08-18 20:46:18 -0400

    Add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://staticmapmaker.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  outDir: "out",
+};
+
+module.exports = config;
