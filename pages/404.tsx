31d84f5a03dfd850b83673c560c939882abb49af
Katy DeCorah <katydecorah@gmail.com>
2024-02-05 20:34:27 -0500

    Remove Sentry

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,8 @@
 import Meta from "../components/meta";
 import styles from "../styles/index.module.scss";
 import Title from "../components/title";
-import * as Sentry from "@sentry/nextjs";
-import { useRouter } from "next/router";
 
 export default function Custom404() {
-  const { asPath } = useRouter();
-  Sentry.withScope(() => {
-    Sentry.setTag("404", true);
-    Sentry.captureException(`404: ${asPath}`);
-  });
   return (
     <>
       <Meta
